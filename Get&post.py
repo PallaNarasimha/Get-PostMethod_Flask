530f37381f3e2e6b3968d1dfbf17dc9708c034d2
palla narasimha <44476773+PallaNarasimha@users.noreply.github.com>
2022-05-27 16:29:28 +0530

    Add files via upload

---
+++ Get&post.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 18:35:31 2022
+
+@author: Admin
+"""
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/test',methods=['GET','POST'])
+
+def login():
+   if request.method == 'POST':
+      req_Json = request.json
+      name = req_Json['name']
+      return jsonify({"Hi"+name})
+   else:
+      
+      return jsonify({"Get Request Called"})
+
+if __name__ == '__main__':
+   app.run(debug = True, port=9000)
+
